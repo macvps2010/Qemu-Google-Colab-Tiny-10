668edc3148ca7e9cb9d44db6e6e74f46a59a59d8
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2022-05-27 17:57:02 +0700

    Update Run1.sh

--- Run1.sh
+++ Run1.sh
@@ -4,20 +4,20 @@ sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw Tiny10.img 64G
 wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
-wget -O Tiny10.iso 'https://udomain.dl.sourceforge.net/project/blissos-dev/Android-Generic/PC/bliss/S/foss/alpha/Bliss-v15.0-Alpha-android_x86_64-OFFICIAL-foss-202112151838-nb-sd-ksu-gearlock-ex_vt-console_all-mesa-drivers-crocus.iso'
+wget -O MACOS.iso 'https://ia800707.us.archive.org/32/items/MacOSXHighSierra10.13/macOS%20HighSierra%2010.13.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 8G \
+  -m 16G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=Tiny10.iso,media=cdrom \
+  -drive file=MACOS.iso,media=cdrom \
   -drive file=Tiny10.img,format=raw \
   -drive file=RTL8139F.iso,media=cdrom \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -cpu core2duo \
-  -smp cores=2 \
+  -cpu ivybridge \
+  -smp cores=6 \
   -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
