ca6ecaf0c82452ab10c723428b72b0175324fd9e
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2022-06-02 08:55:24 +0700

    Update Run1.sh

--- Run1.sh
+++ Run1.sh
@@ -2,18 +2,16 @@ sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
-qemu-img create -f raw Tiny10.img 64G
-wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
-wget -O MACOS.iso 'https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso'
+qemu-img create -f raw Ubuntu.img 64G
+wget -O Ubuntu.iso 'https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 8G \
+  -m 4G \
   -cpu EPYC \
   -boot order=d \
-  -drive file=MACOS.iso,media=cdrom \
-  -drive file=Tiny10.img,format=raw \
-  -drive file=RTL8139F.iso,media=cdrom \
+  -drive file=Ubuntu.iso,media=cdrom \
+  -drive file=Ubuntu.img,format=raw \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
