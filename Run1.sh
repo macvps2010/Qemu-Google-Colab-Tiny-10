fa59f6198e95058b8418a42917beba92491ccd37
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2022-06-07 15:54:50 +0700

    Update Run1.sh

--- Run1.sh
+++ Run1.sh
@@ -1,3 +1,10 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip &>/dev/null &
+unzip ngrok-stable-linux-amd64.zip1 &>/dev/null &
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3388 &>/dev/null &
+./ngrok tcp 3388 &>/dev/null &
 sudo apt-get update && apt-get install qemu -y
 sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
@@ -6,7 +13,7 @@ qemu-img create -f raw Ubuntu.img 64G
 wget -O Ubuntu.iso 'https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso'
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
-  -m 4G \
+  -m 8G \
   -cpu EPYC \
   -boot order=d \
   -drive file=Ubuntu.iso,media=cdrom \
@@ -15,7 +22,7 @@ sudo qemu-system-x86_64 \
   -device usb-tablet \
   -vnc :0 \
   -cpu core2duo \
-  -smp cores=4 \
+  -smp cores=6 \
   -device rtl8139,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
   
