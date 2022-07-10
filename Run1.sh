f099bcf72deba16df9c762197bf9858073857c11
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2022-07-10 19:33:48 +0700

    Update Run1.sh

--- Run1.sh
+++ Run1.sh
@@ -10,6 +10,7 @@ sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw Ubuntu.img 64G
+qemu-img create -f raw ABC.img 64G
 wget -O Ubuntu.iso 'https://ia803409.us.archive.org/18/items/androidtv-x86/ATV-9-X86-Techinfo-k4.19.105-64bit.iso'
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
@@ -18,6 +19,7 @@ sudo qemu-system-x86_64 \
   -boot order=d \
   -drive file=Ubuntu.iso,media=cdrom \
   -drive file=Ubuntu.img,format=raw \
+  -drive file=ABC.img,format=raw \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
