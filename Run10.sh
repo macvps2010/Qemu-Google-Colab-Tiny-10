6d7e790b0ed90a1993ab0a7ff8bb1834b934f716
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2022-07-26 19:41:25 +0700

    Update Run10.sh

--- Run10.sh
+++ Run10.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install -y qemu-system-x86-64
 wget -O RTL8139F.iso 'https://drive.google.com/uc?export=download&id=1wDL8vo9mmYKw1HKXZzaYHoKmzSt_wXai'
-wget -O Tiny10.qcow2 'https://www.dropbox.com/s/v3gak59mvkqb7g6/Tiny10.qcow2?dl=1'
+wget -O Nico.iso 'https://github.com/pearOS-archlinux/iso/releases/download/v22.06-release-candidate/pearOS-NiceC0re-2022.07-x86_64.iso'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
@@ -18,3 +18,9 @@ sudo qemu-system-x86_64 \
   -vga std \
   -device e1000,netdev=n0 -netdev user,id=n0 \
   -accel tcg,thread=multi \
+      
+      
+      
+      
+      
+qemu-system-x86_64 -m 1200M -hda a.qcow2 -vga std -cpu core2duo -smp sockets=1,cores=8,threads=1 -device e1000,netdev=n0 -netdev user,id=n0 -soundhw hda -vnc 127.0.0.1:23
