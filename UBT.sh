69eb309a97a06a3c83437e9f515746f4b9e96dc5
macvps2010 <101693288+macvps2010@users.noreply.github.com>
2023-04-29 14:32:21 +0700

    Update UBT.sh

--- UBT.sh
+++ UBT.sh
@@ -10,7 +10,7 @@ sudo apt install qemu-utils -y
 sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw Ubuntu.img 64G
-wget -O Ubuntu.iso 'https://download2438.mediafire.com/640gq3xhxd5g/jxyc8n25s1asati/tiny10+21H2+x64+2209.iso'
+wget -O Ubuntu.iso 'https://doc-0g-c4-docs.googleusercontent.com/docs/securesc/vr1v0ldjjhfelg39ba1fjfopvdifkfac/9d9ovo68edj4jf9k0k13euekh9kum073/1682753475000/09155415212261886625/04051407595031969789/1k51c-q7qEQ_XmxLt8vx7DLtoKucsA566?e=download&ax=ALy03A6M-jf3QEw9Rl99KI1iNc0g1YtaWwsEFlQaJrin_YZAkiYCuA-Iu-PivmVLkpeKbCC6gULufA14UEpvlH2wG2aLyzdk7rIw7RiIZXi1xTQso91EhuaUigVgbzHHZYrgVmQWufQOvPPMGs_Sz50SlF0QZRx1JENqhwC1dTuM4jbl8plZIIyz0PRYlTI8pud4ZPR_V-UQhVJnkBGpeSq9i3es131J7loutBu5hpNjOBsBnxQzWvcYNw5AqjELfQZN0Xbt9LHScXXLP7faxfEYHvXvyFoEv6yRSUhr0VEcTyqfybkQkRe8UY2D-Xs68_OCvTNVnAeGVjRdYkybStB8MFxiB10bkTvWxbRivMjo3fc9HKVdC2QyzcW-9S8ENEewSxiTeUzc1Ntz5F6Q3qXV_mSl6b3VA5GS9CQ1Y9LCWMjnFcSLarKAVO2UXK0EGWB-e4Vv5bdvnyPr22afq2VDBNnGoJ0DFWtoarW9SmJuosEdGHEynfcgejmDu6F8YdqTwO8Ymt9C-yy5T800pJuJc0sjyJlAxbIJ0YEPjUSMCp3061EollKL3fSZvphpmEbdbYKCzSHBa4bZJrtG1-CqFIs-rybx4b-llt7oYhi-OBI_sSZ4h2DM0hrJLl9H40FFk6ZreR_o0KDBCdcPulxeawybmDw8bWcA-3iOL4Lbt_gOGkqMkts4VDYxBRnoCNILD7z9Hsnx_1H9OCrcmYQvwOEz7qOXsW8OZKXvfQbjPQtHis6QdqSSsMlvvvQtitmwS_Wjsz97GwYcPgrPHmKW1wLrRgCUp-_NWZkHGEfiEWYJd9W4aT33XWucSVchD2NTZazVu_RlJPL1UbxlmRc8j7JoVe-7kLNIfE_1I7tANAHTJMx41bM0txhRLmp9NBOEySRS4rNOWUxDbIgjQeWfik7CZ3qpS9sIOxamUarMCNvIJYBdggwv1aJUNnK0bCx3x8nTn0g&uuid=65525567-490b-463c-b17f-a0e5c94b8105&authuser=0&nonce=f3a6akorjs0gk&user=04051407595031969789&hash=pg23hdurk8kmivs9qsos9egm06dljk6q'
 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 sudo qemu-system-x86_64 \
